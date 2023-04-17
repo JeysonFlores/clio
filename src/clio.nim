057b20812db5bb468df741d31c27f1c55ea90bca
JeysonFlores <jeyson.flores@hotmail.com>
2023-04-17 00:07:56 -0700

    Added public initializers (sync/async) for Clio app

--- src/clio.nim
+++ src/clio.nim
@@ -1,6 +1,6 @@
 # -*- utf-8 -*-
 import std/tables
-
+import std/async
 type
   Opt* = object
   Arg* = object
@@ -9,18 +9,21 @@ type
     args*: OrderedTable[string, Arg]
     flags*: OrderedTable[string, Flag]
     options*: OrderedTable[string, Opt]
-  Clio* = ref object
+  Clio[T] = ref object
     name*: string
-    description*: string
-    version*: string
-    args*: OrderedTable[string, Arg]
-    flags*: OrderedTable[string, Flag]
-    options*: OrderedTable[string, Opt]
-    cb: proc(ctx: Context): void
+    #description*: string
+    #version*: string
+    #args*: OrderedTable[string, Arg]
+    #flags*: OrderedTable[string, Flag]
+    #options*: OrderedTable[string, Opt]
+    cb: proc(ctx: Context): T
 
 
-proc newApp*(_: typedesc[Clio], name: string): Clio =
-  Clio(name: name)
+proc newClioApp*(name: string): Clio[void] =
+  Clio[void](name: name)
+
+proc newAsyncClioApp*(name: string): Clio[Future[void]] =
+  Clio[Future[void]](name: name)
 
 proc build*(self: Clio) = 
   self.name = "OWO"
--- tests/test_clio.nim
+++ tests/test_clio.nim
@@ -1,10 +1,15 @@
-import std/with
+import std/[with]
 import unittest
 import clio
 
 test "app":
-  var app = Clio.newApp("Example")
+  var app = newClioApp("Example")
+  var app2 = newAsyncClioApp("Async Example")
 
   with app:
+    build
+    start
+
+  with app2:
     build
     start
