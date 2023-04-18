a65be3536db782cf5fc1f4d9dd2ac17794707cf9
JeysonFlores <jeyson.flores@hotmail.com>
2023-04-17 21:03:51 -0700

    Message stylishing logic done

--- src/clio/message.nim
+++ src/clio/message.nim
@@ -1,14 +1,35 @@
+import std/terminal
+
+
 type
+  MessageStyle* = object
+    color*: ForegroundColor
+    text*: Style
+    marker*: string
   Message* = object
 
-proc info*(_: typedesc[Message], msg: string) =
-  echo msg
 
-proc alert*(_: typedesc[Message], msg: string) =
-  echo msg
+proc stylize*(msg: string, style: MessageStyle) =
+  stdout.styledWriteLine(style.color, style.text, style.marker, " ", msg)
 
-proc success*(_: typedesc[Message], msg: string) = 
-  echo msg
 
-proc warning*(_: typedesc[Message], msg: string) =
-  echo msg
+const alertStyle = MessageStyle(color: fgBlue, text: styleBright, marker: "•")
+const infoStyle = MessageStyle(color: fgYellow, text: styleBright, marker: "•")
+const successStyle = MessageStyle(color: fgGreen, text: styleBright, marker: "•")
+const warningStyle = MessageStyle(color: fgRed, text: styleBright, marker: "•")
+
+
+proc alert*(_: typedesc[Message], msg: string,
+    style: MessageStyle = alertStyle) =
+  msg.stylize(style)
+
+proc info*(_: typedesc[Message], msg: string, style: MessageStyle = infoStyle) =
+  msg.stylize(style)
+
+proc success*(_: typedesc[Message], msg: string,
+    style: MessageStyle = successStyle) =
+  msg.stylize(style)
+
+proc warning*(_: typedesc[Message], msg: string,
+    style: MessageStyle = warningStyle) =
+  msg.stylize(style)
