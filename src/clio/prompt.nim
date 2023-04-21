20cf8c14b6fc40c9da53c0d5af4f02e961ea976e
JeysonFlores <jeyson.flores@hotmail.com>
2023-04-20 23:38:02 -0700

    Decoder callback sugar'ed

--- src/clio/prompt.nim
+++ src/clio/prompt.nim
@@ -1,21 +1,20 @@
-import std/options
+import std/[options, sugar]
 
 
 type
   Prompt* = object
 
-proc input*(_: typedesc[Prompt], msg: string, default: Option[string]): Option[string] =
+proc input*(_: typedesc[Prompt], msg: string, default: Option[string] = none(string)): Option[string] =
   echo msg
 
-proc number*(_: typedesc[Prompt], msg: string, default: Option[int]): Option[int] =
+proc number*(_: typedesc[Prompt], msg: string, default: Option[int] = none(int)): Option[int] =
   echo msg
 
-proc secret*(_: typedesc[Prompt], msg: string, default: Option[string]): Option[string] =
+proc secret*(_: typedesc[Prompt], msg: string, default: Option[string] = none(string)): Option[string] =
   echo msg
 
 proc confirm*(_: typedesc[Prompt], msg: string): bool =
   echo msg
 
-proc parse*[T](_: typedesc[Prompt], msg: string, decoder: proc(
-    x: string): T): Option[T] =
+proc parse*[T](_: typedesc[Prompt], msg: string, decoder: (string) -> T, default: Option[T] = none(T)): Option[T] =
   echo msg
