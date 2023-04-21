180fcfbadfd0559843f7ce021bcaa755bd451690
JeysonFlores <jeyson.flores@hotmail.com>
2023-04-21 00:00:00 -0700

    Prompt functions formatted

--- src/clio/prompt.nim
+++ src/clio/prompt.nim
@@ -4,17 +4,34 @@ import std/[options, sugar]
 type
   Prompt* = object
 
-proc input*(_: typedesc[Prompt], msg: string, default: Option[string] = none(string)): Option[string] =
+proc input*(
+    _: typedesc[Prompt],
+    msg: string,
+    default: Option[string] = none(string)
+    ): Option[string] =
   echo msg
 
-proc number*(_: typedesc[Prompt], msg: string, default: Option[int] = none(int)): Option[int] =
+proc number*(
+    _: typedesc[Prompt],
+    msg: string,
+    default: Option[int] = none(int)
+    ): Option[int] =
   echo msg
 
-proc secret*(_: typedesc[Prompt], msg: string, default: Option[string] = none(string)): Option[string] =
+proc secret*(
+      _: typedesc[Prompt],
+      msg: string,
+      default: Option[string] = none(string)
+      ): Option[string] =
+  echo msg
+
+proc parse*[T](
+      _: typedesc[Prompt],
+      msg: string,
+      decoder: (string) -> T,
+      default: Option[T] = none(T)
+      ): Option[T] =
   echo msg
 
 proc confirm*(_: typedesc[Prompt], msg: string): bool =
-  echo msg
-
-proc parse*[T](_: typedesc[Prompt], msg: string, decoder: (string) -> T, default: Option[T] = none(T)): Option[T] =
   echo msg
